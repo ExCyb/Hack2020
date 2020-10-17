fcf030cd0c2158ec441ac1b02d679c3599614e53
spyptev <spyptev@artec-group.com>
2020-10-17 21:54:50 +0300

    Alice sevice added

---
+++ alice/index.js
@@ -0,0 +1,73 @@
+const express = require('express');
+
+const port = process.env.PORT || 3000;
+const app = express();
+
+app.use(express.json());
+
+app.post('/', function (req, res) {
+
+  if (req.body.request.command == "no text")
+  {
+    res.json({
+      version: req.body.version,
+      session: req.body.session,
+      response: {
+        text: "",
+        end_session: false,
+      },
+    });
+  }
+
+  else if (req.body.request.command == "no version")
+  {
+    res.json({
+      session: req.body.session,
+      response: {
+        text: 'Сейчас свободны 3 парковочных места',
+        end_session: true,
+      },
+    });
+  }
+
+  else if (req.body.request.command == "no session")
+  {
+    res.json({
+      version: req.body.version,
+      response: {
+        text: 'Сейчас свободны 3 парковочных места',
+        end_session: true,
+      },
+    });
+  }
+
+  else if (req.body.request.command == "end session")
+  {
+    res.json({
+      version: req.body.version,
+      session: req.body.session,
+      response: {
+        text: 'Сейчас свободны 3 парковочных места',
+        end_session: true,
+      },
+    });
+  }
+
+  else 
+  {
+    res.json({
+      version: req.body.version,
+      session: req.body.session,
+        response: {
+        text: 'Сейчас свободны 3 парковочных места',
+        end_session: true,
+        },
+    })
+  ;}
+});
+
+app.use('*', function (req, res) {
+  res.sendStatus(404);
+});
+
+app.listen(port);
