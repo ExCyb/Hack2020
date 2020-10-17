a352bf7aa16548f214296783e614e2a51541de94
Vitalii Kim <vitaliikim992@gmail.com>
2020-10-18 01:08:00 +0400

    back initial

---
+++ back/parkingsportdetection/src/test/java/org/kiborgi/parkingsport/ParkingsportdetectionApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kiborgi.parkingsport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingsportdetectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
